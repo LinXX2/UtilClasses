ea6121ef3af52310761095efa5fbe6a630436064
LinXX <niklas.kauert@gmail.com>
2016-11-12 15:36:05 +0100

    e

--- src/me/linxx/minigameapi/MinigameAPI.java
+++ src/me/linxx/minigameapi/MinigameAPI.java
@@ -3,6 +3,9 @@ package me.linxx.minigameapi;
 import java.util.ArrayList;
 
 import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -12,6 +15,7 @@ public class MinigameAPI {
 	private ArrayList<Player> players = new ArrayList<>();
 	private int maxplayers;
 	private int minplayers;
+	private int startinseconds;
 	
 	private GameState state;
 
@@ -21,14 +25,27 @@ public class MinigameAPI {
 	
 	private Plugin pl;
 	
-	public MinigameAPI() {
-		
+	public MinigameAPI(int maxplayers, int minplayers, int startinseconds, GameState state, ArrayList<Location> spawns, Location lobbyspawn, Location endspawn, Plugin pl) {
+		this.maxplayers = maxplayers;
+		this.minplayers = minplayers;
+		this.startinseconds = startinseconds;
+		this.state = state;
+		this.spawns = spawns;
+		this.lobbyspawn = lobbyspawn;
+		this.endspawn = endspawn;
+		this.pl = pl;
 	}
 	
 	
 	
 	
 	
+	
+	
+	
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
 	
 	
 	private enum GameState {
@@ -39,4 +56,77 @@ public class MinigameAPI {
 		CLOSE;
 		
 	}
+	
+	private class StartCMD implements CommandExecutor {
+
+		@Override
+		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+			
+			return false;
+		}
+		
+		
+	}
+
+	public int getMaxplayers() {
+		return maxplayers;
+	}
+
+	public void setMaxplayers(int maxplayers) {
+		this.maxplayers = maxplayers;
+	}
+
+	public int getMinplayers() {
+		return minplayers;
+	}
+
+	public void setMinplayers(int minplayers) {
+		this.minplayers = minplayers;
+	}
+
+	public int getStartinseconds() {
+		return startinseconds;
+	}
+
+
+	public void setStartinseconds(int startinseconds) {
+		this.startinseconds = startinseconds;
+	}
+
+
+	public GameState getState() {
+		return state;
+	}
+
+
+	public void setState(GameState state) {
+		this.state = state;
+	}
+
+
+	public ArrayList<Location> getSpawns() {
+		return spawns;
+	}
+
+	public void setSpawns(ArrayList<Location> spawns) {
+		this.spawns = spawns;
+	}
+
+	public Location getLobbyspawn() {
+		return lobbyspawn;
+	}
+
+	public void setLobbyspawn(Location lobbyspawn) {
+		this.lobbyspawn = lobbyspawn;
+	}
+
+	public Location getEndspawn() {
+		return endspawn;
+	}
+
+	public void setEndspawn(Location endspawn) {
+		this.endspawn = endspawn;
+	}
+	
+	
 }
